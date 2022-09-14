789565a20534b3d94cc6a410f899ca88e6180b07
0xSaiya <hazrid93@hotmail.com>
2022-09-15 02:05:24 +0800

    Update maybe.ts

--- src/utils/maybe.ts
+++ src/utils/maybe.ts
@@ -1,11 +1,22 @@
-class MaybeItem<T> {
+import EventEmitter = require('events');
+
+/**
+ * This class is used as a wrapper for chain of operations where any error are unimportant.
+ * Error will just be swallowed and the last result pre-exception will be return. This is
+ * beneficial to avoid codes that has multiple null/undefined checks.
+ * 
+ * @author Hazrid Azad - (0xSaiya)
+ */
+class Maybe<T> extends EventEmitter{
     private input: T;
+    private readonly ERROR_EVENT: string = `error`;
 
     /**
-     * @constructor constructor for MaybeItem
+     * @constructor constructor for Maybe
      * @param {T} input of any type
      */
-    private constructor(_input: T){
+    constructor(_input: T){
+        super();
         this.input = _input;
     }
 
@@ -18,21 +29,23 @@ class MaybeItem<T> {
      * @returns {Maybe<T>} returns the response of binded function that processed the input wrapped as MaybeItem
      */ 
     public static start<T>(_input: T){
-        return new MaybeItem<T>(_input);
+        return new Maybe<T>(_input);
     }
-    
+
     public bind(_ :(input: T)=> T) {
         try{
             const result = _(this.input);
             if( result == null ) {
                 return this;
             } else {
-                return new MaybeItem<T>(result);
+                return new Maybe<T>(result);
             }
         } catch (ex) {
+            console.error(`Fail to process one of the binded method, reason: ${ex.message}`);
+            this.emit(this.ERROR_EVENT, `Fail to process one of the binded method, reason: ${ex.message}`);
             return this;
         }
     }
 }
 
-export default MaybeItem;
+export default Maybe;
